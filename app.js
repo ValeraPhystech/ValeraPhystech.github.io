3b827a50bff2ca9e1e1d844fc4c7db90439669f2
ValeraPhystech <lobov98omsk@gmail.com>
2017-11-22 15:04:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿function addelement(s) {
+    var elem1 = $('<li><span></span> <button class="delete-button">Удалить</button></li>');
+    $('span', elem1).text(s);
+    $('#root ul').append(elem1);
+    $('.delete-button', elem1).click(function(ev){$(this).parent().remove()});
+}
+$(function(){
+    $('#root').append('<ul></ul>');
+    $('#root').append('<input id="add_task_input"> <button id="add_task">Добавить</button>');
+    $('#add_task').click(function(){addelement($('#add_task_input').val())});
+    addelement('Сделать задание #3 по web-программированию');
+}) 
